d5f682474c13b0ed87d36affdf3a0346f47d4c38
Tribeoftech <gratefulgrowing333@gmail.com>
2023-01-25 17:19:28 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Prints message to terminal
+ *
+ * Return: 0 if succesful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
