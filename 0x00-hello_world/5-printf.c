1eaec0d5bfd0bfbbbdae198034699f899a48ab95
Tribeoftech <gratefulgrowing333@gmail.com>
2023-01-25 18:50:08 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints message to terminal
+ *
+ * Return: 0 if succesful
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+ return(0);
+}
