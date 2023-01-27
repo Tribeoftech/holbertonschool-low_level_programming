59e34d19316aca9097c17413b1ae3fa2eff010f3
Tribeoftech <gratefulgrowing333@gmail.com>
2023-01-26 21:38:55 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers 0-10.
+ * 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+		putchar((d % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
