613237d65ebb2b88c9d2f61a9af5ac2806f6f0fd
Tribeoftech <gratefulgrowing333@gmail.com>
2023-01-27 20:16:27 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Task 2 program that prints the alphabet x10
+ *
+ * Return: 0 (Successful)
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+	_putchar('\n');
+	}
+}
