9f8e61d3cb555ce628615a73b2bdb3493240a8f2
Tribeoftech <gratefulgrowing333@gmail.com>
2023-01-28 14:49:48 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - see description
+ * @c: number
+ * Description: Print alpha 10 times
+ * Return: Always 0 (Success)
+*/
+int _isalpha(int c);
+{
+for (c <= 'a')
+{
+return (1);
+}
+else
+}
+return (0)
