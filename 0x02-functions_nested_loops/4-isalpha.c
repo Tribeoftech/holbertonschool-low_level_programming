2b216d4def98d6a75add96843f19f81d7ba04b00
Tribeoftech <gratefulgrowing333@gmail.com>
2023-01-28 19:26:28 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - prints alphabet in lowercase
+ * main-Entry- prints alphabet in lowercase
  * _isalpha- checks code
  *
  * Return: 0 if runs successful
