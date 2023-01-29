365607107233a0f9088c0d53cf869009d4c30831
Tribeoftech <gratefulgrowing333@gmail.com>
2023-01-28 17:38:59 -0800

    3-islower.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,4 +15,12 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+int add(int x, int y);
+void print_to_98(int n);
+void times_table(void);
+void jack_bauer(void);
 #endif
