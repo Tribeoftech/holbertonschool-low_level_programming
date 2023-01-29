b9a52f46117b025f8bcb889e80df0497a3db412a
Tribeoftech <gratefulgrowing333@gmail.com>
2023-01-29 15:41:30 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - success
- *
  * print_alphabet - prints alphabet in lowercase
  *
  * Return: 0 if succesful
  */
 
-int print_alphabet(void);
+void print_alphabet(void)
 {
 int c;
+
 for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
 _putchar('\n');
-return (0);
 }
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-/**
- * 
- *
- *
- *
- *
- */
-
 int _putchar(char);
-int print_alphabet(void);
+void print_alphabet(void);
 int print_alphabet_x10(void);
 int _islower(char c);
 int _isalpha(int c);
@@ -24,5 +10,3 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-#endif
-
