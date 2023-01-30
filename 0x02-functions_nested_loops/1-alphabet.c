29f5d7312ea670940837bf3ba8039bf9a942e5cb
Tribeoftech <gratefulgrowing333@gmail.com>
2023-01-29 17:04:38 -0800

    3-islower.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,7 +8,7 @@
 
 void print_alphabet(void)
 {
-int c;
+char c;
 
 for (c = 'a'; c <= 'z'; c++)
 {
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
-#include <stdio.h>
  /**
- *_islower - Task 3 program checks if character is lowercase`
- *@c: integer to be checked
+ * _islower - Task 3 program checks if character is lowercase`
  *
- *Return: 1 if c is lowercase, 0 otherwise
+ * @c: integer to be checked
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
