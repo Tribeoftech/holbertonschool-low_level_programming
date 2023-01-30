0a6109133c68c0660edaed95cb074eda2c4b70ac
Tribeoftech <gratefulgrowing333@gmail.com>
2023-01-30 14:23:17 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,32 +12,60 @@
 */
 void times_table(void)
 {
-int row;
-int column;
-int result;
+	int x, y, z;
 
-for (row = 0; row <= 0; row++)
-{
-for (column = 0; column <= 0; column++)
-{
-result = (row * column);
-
-if (column == 0)
-{
-_putchar('0');
-}
-else if (result <= 9)
-{
-_putchar(',');
-_putchar('0' + result);
-}
-else if (result > 9)
-{
-_putchar(',');
-_putchar('0' + (result / 10));
-_putchar('0' + (result % 10));
-}
-}
-_putchar('\n');
-}
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			z = x * y;
+			if (z < 10)
+			{
+				_putchar(z + '0');
+			}
+			else if (z > 9)
+			{
+				_putchar('1');
+				_putchar((z % 10) + '0');
+			}
+			else if (z > 19)
+			{
+				_putchar('2');
+				_putchar((z % 10) + '0');
+			}
+			else if (z > 29)
+			{
+				_putchar('3');
+				_putchar((z % 10) + '0');
+			}
+			else if (z > 39)
+			{
+				_putchar('4');
+				_putchar((z % 10) + '0');
+			}
+			else if (z > 49)
+			{
+				_putchar('5');
+				_putchar((z % 10) + '0');
+			}
+			else if (z > 59)
+			{
+				_putchar('6');
+				_putchar((z % 10) + '0');
+			}
+			else if (z > 69)
+			{
+				_putchar('7');
+				_putchar((z % 10) + '0');
+			}
+			else if (z > 79)
+			{
+				_putchar('8');
+				_putchar((z % 10) + '0');
+			}
+			_putchar(44);
+			_putchar(32);
+		}
+	_putchar(10);
+	}
 }
