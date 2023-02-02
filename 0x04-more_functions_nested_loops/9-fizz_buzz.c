78235a12f252c072462f1e40aa2da3203952aebe
Tribeoftech <gratefulgrowing333@gmail.com>
2023-02-02 14:18:35 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Fizz buzz interview question
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 1;
+
+	for (; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		if (x % 5 == 0 && x % 3 != 0)
+		{
+			printf("Buzz");
+			if (x != 100)
+			{
+				printf(" ");
+			}
+		}
+		if (x % 5 == 0 && x % 3 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		if (x % 5 != 0 && x % 3 != 0)
+		{
+			printf("%i ", x);
+		}
+	}
+	printf("\n");
+	return (0);
+}
