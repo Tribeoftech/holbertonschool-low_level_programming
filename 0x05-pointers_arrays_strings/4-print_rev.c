12cda5c01b52e67d8b06745e6f3319076500d0de
Tribeoftech <gratefulgrowing333@gmail.com>
2023-02-03 15:23:22 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * Print_rev - prints string in reverse.
+ * @s: pointer to the string to be printed.
+ *
+ */
+void print_rev(char *s)
+{
+	printf("%s/n", s);
+	printf("rev");
+}
