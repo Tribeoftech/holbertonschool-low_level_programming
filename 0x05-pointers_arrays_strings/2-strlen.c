43aedee40c23a9251cf2522856e4cc7dd4d78c0a
Tribeoftech <gratefulgrowing333@gmail.com>
2023-02-03 09:07:31 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+# include "main.h"
+# include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: char
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+	length++;
+	}
+	return (length);
+}
