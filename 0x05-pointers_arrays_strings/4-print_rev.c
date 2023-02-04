f39a3a57e1eebafde850e62806779843b29218e5
Tribeoftech <gratefulgrowing333@gmail.com>
2023-02-04 12:40:12 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,15 +6,20 @@
  */
 void print_rev(char *s)
 {
-	if (*s)
+	int len = 0;
+	int i;
+
+	while (*s != '\0')
 	{
-	print_rev(s + 1);
-	putchar(*s);
+		len++;
+		s++;
 	}
+	s--;
+
+	for (i = len; i > 0; i--)
 	{
-	putchar("/n");
+		putchar(*s);
+		s--;
 	}
+	putchar('\n');
 }
-
-
-
