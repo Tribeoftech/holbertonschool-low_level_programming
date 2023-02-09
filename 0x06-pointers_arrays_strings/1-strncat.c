dddc654e0d3d692a5b406ce2fd9947957c8b9bc9
Tribeoftech <gratefulgrowing333@gmail.com>
2023-02-09 13:55:13 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strncat - Combines two strings
+ * @dest: One of the two strings to combine
+ * @src: One of the two strings to combine
+ * @n: amount of bytes
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int x = 0, y = 0;
+
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+
+	while (y < n)
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+
+	dest[x] = '\0';
+	return (dest);
+}
