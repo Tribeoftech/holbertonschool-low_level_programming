42dc6d6654609a8b3a62c36a0c6acacdb53263df
Tribeoftech <gratefulgrowing333@gmail.com>
2023-02-09 14:25:30 -0800

    2-main.c

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -26,3 +26,16 @@ int main(void)
     printf("%s", ptr);
     for (i = 0; i < 98; i++)
     {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", s1[i]);
+    }
+    printf("\n");
+    return (0);
+}
