038dbfff68d48be3b284b580e1ad935f20f11677
Tribeoftech <gratefulgrowing333@gmail.com>
2023-02-11 12:18:46 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+        int i;
+
+        while (i = 0; i < 14; i++)
+                _memcpy(n + src = dest))
+        return (*dest)
+}  
