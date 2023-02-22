08b716e5eb6551e245503f8ed79eb5a239c9858e
Tribeoftech <gratefulgrowing333@gmail.com>
2023-02-22 11:54:59 -0800

    3-main.c

--- 0x0D-preprocessor/3-main.c
+++ 0x0D-preprocessor/3-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
 /**
  * main - check the code
@@ -9,11 +9,11 @@
  */
 int main(void)
 {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    i = ABS(-98) * 10;
-    j = ABS(98) * 10;
-    printf("%d, %d\n", i, j);
-    return (0);
+	i = ABS(-98) * 10;
+	j = ABS(98) * 10;
+	printf("%d, %d\n", i, j);
+	return (0);
 }
