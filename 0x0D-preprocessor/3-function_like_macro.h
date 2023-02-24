533b8e166eb5ee92a59f885c5df00ff03ef91682
Tribeoftech <gratefulgrowing333@gmail.com>
2023-02-24 15:56:39 -0800

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/* inclues */
+
+#include <unistd.h>
+#include <stdio.h>
+
+/* function like macro */
+
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
