e372c490fe82ef71b1b385c5f3e7489d79b84973
Tribeoftech <gratefulgrowing333@gmail.com>
2023-03-03 15:02:46 -0800

    every dog has its day

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,35 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - Prints a struct dog
+ * @d: Pointer to struct dog
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name != NULL)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+
+		printf("Age: %f\n", d->age);
+
+		if (d->owner)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
+	}
+}
+
