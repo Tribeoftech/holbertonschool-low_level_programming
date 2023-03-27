fd8f26f0a6ed4a229a919afaf6620d9fefa77ebe
Tribeoftech <gratefulgrowing333@gmail.com>
2023-03-26 17:02:51 -0700

    hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,26 +1,48 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - this function gets a value from a table
- * @ht: hash table
- * @key: key to get the value from
- * Return: value or null
+ * hash_table_set -  adss an element to hash table
+ * if collision adds new node at begining of the list
+ * @ht: hashtable to add/update
+ * @key: The key
+ * @value: value for the key
+ * Return: 1 if success, 0 if failure
  */
 
-char *hash_table_get(const hash_table_t *ht, const char *key)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int my_index;
+	hash_node_t *search, *new_node = NULL;
+	unsigned long int index;
+	char *new_value = NULL;
 
-	if (ht == NULL || key == NULL)
-		return (NULL);
-	my_index = key_index((const unsigned char *) key, ht->size);
-	while (ht->array[my_index] != NULL)
+	if (!ht || !key || !value)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	search = ht->array[index];
+
+	while (search != NULL)
 	{
-		if (strcmp((ht->array[my_index])->key, key) == 0)
+		if (strcmp(search->key, key) == 0)
 		{
-			return ((ht->array[my_index])->value);
+			new_value = strdup(value);
+			free(search->value);
+			search->value = new_value;
+			return (1);
 		}
-		ht->array[my_index] = (ht->array[my_index])->next;
+		search = search->next;
 	}
-	return (NULL);
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }
